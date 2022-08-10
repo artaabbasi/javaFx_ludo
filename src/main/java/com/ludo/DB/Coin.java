492741652fc3953f75c82038117c00d418b2d48a
artaNova <arta1382@gmail.com>
2022-08-10 15:06:10 +0430

    game logic finished

--- src/main/java/com/ludo/DB/Coin.java
+++ src/main/java/com/ludo/DB/Coin.java
@@ -1,6 +1,7 @@
 package com.ludo.DB;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Coin implements Serializable {
     private static final long serialVersionUID = 1234567L;
@@ -8,5 +9,98 @@ public class Coin implements Serializable {
     public Game game;
     public Integer type;
     public Integer position = 0;
-    public Integer abs_position = 0;
+
+
+    public int get_abs_position(Integer pos){
+        if( type.equals(1)){
+            Integer abs = pos + 11;
+            if (abs > 44){
+                abs = abs - 44;
+            }
+            if (pos > 43){
+                abs = 100 + (pos - 43);
+            }
+            return abs;
+        }
+        else if( type.equals(4)){
+            Integer abs = pos + 22;
+            if (abs > 44){
+                abs = abs - 44;
+            }
+            if (this.position > 43){
+                abs = 100 + (this.position - 43);
+            }
+            return abs;
+        }
+        else if( type.equals(2)){
+            Integer abs = pos + 34;
+            if (abs > 44){
+                abs = abs - 44;
+            }
+            if (this.position > 43){
+                abs = 100 + (pos - 43);
+            }
+            return abs;
+        }
+        else {
+            Integer abs = pos;
+            if (pos > 43){
+                abs = 100 + (pos - 43);
+            }
+            return abs;
+        }
+    }
+    public int abs_position(){
+        return get_abs_position(this.position);
+    }
+    public Boolean can_move(Integer position, ArrayList<Coin> coins) {
+        if (position > 47) {
+            return false;
+        }
+        for (Coin coin : coins) {
+            if (position > 43) {
+                if (coin.type.equals(this.type) && coin.position.equals(position)) {
+                    return false;
+                }
+            } else {
+                Integer coins_abs = coin.abs_position();
+                if (coins_abs.equals(this.get_abs_position(position))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public Boolean can_move_by_dice(Integer dice, ArrayList<Coin> coins) {
+        Integer positionnnn = this.position;
+        if (positionnnn == 0 ) {
+            if (dice.equals(6)){
+                positionnnn = 1;
+            }
+            else {
+                return false;
+            }
+        }else {
+            positionnnn = positionnnn + dice;
+        }
+
+        if (positionnnn > 47){
+            return false;
+        }
+        for (Coin coin: coins) {
+            if (positionnnn > 43) {
+                if (coin.type.equals(this.type) && coin.position.equals(positionnnn)) {
+                    return false;
+                }
+            }else{
+                Integer coins_abs = coin.abs_position();
+                if (coins_abs.equals(this.get_abs_position(positionnnn))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+
+    }
 }
--- src/main/java/com/ludo/ludo/GameController.java
+++ src/main/java/com/ludo/ludo/GameController.java
@@ -4,6 +4,7 @@ import com.ludo.DB.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
@@ -11,6 +12,7 @@ import javafx.scene.shape.Circle;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.function.Consumer;
 
 public class GameController {
     @FXML
@@ -20,6 +22,38 @@ public class GameController {
     private Label blue_nickname;
     @FXML
     private Label coin_1_text;
+    @FXML
+    private Label blue_coin_1;
+    @FXML
+    private Label blue_coin_2;
+    @FXML
+    private Label blue_coin_3;
+    @FXML
+    private Label blue_coin_4;
+    @FXML
+    private Label green_coin_1;
+    @FXML
+    private Label green_coin_2;
+    @FXML
+    private Label green_coin_3;
+    @FXML
+    private Label green_coin_4;
+    @FXML
+    private Label red_coin_1;
+    @FXML
+    private Label red_coin_2;
+    @FXML
+    private Label red_coin_3;
+    @FXML
+    private Label red_coin_4;
+    @FXML
+    private Label yellow_coin_1;
+    @FXML
+    private Label yellow_coin_2;
+    @FXML
+    private Label yellow_coin_3;
+    @FXML
+    private Label yellow_coin_4;
     @FXML
     private Label coin_2_text;
     @FXML
@@ -47,10 +81,33 @@ public class GameController {
     private ArrayList<Player> players;
     private ArrayList<Coin> coins;
 
-    private void setTurn(){
+    private void setTurn() throws IOException {
         Boolean flag = false;
         if (turn == 0){
             flag = true;
+        }
+        ArrayList<Coin> this_type_coins = new ArrayList<Coin>();
+        for (Coin c : coins) {
+            if (c.type.equals(turn)) {
+                this_type_coins.add(c);
+            }
+        }
+        Boolean win = false;
+        for (Coin c : this_type_coins) {
+            if (c.position > 43) {
+                win = true;
+            }else {
+                win = false;
+                break;
+            }
+        }
+        if (win){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Winner");
+            alert.setHeaderText("Player " + turn + " won the game!");
+            alert.showAndWait();
+            LudoApplication scene = new LudoApplication();
+            scene.onChangeScene("game_start_view.fxml");
         }
         while (true) {
             for (Player player : players) {
@@ -66,27 +123,31 @@ public class GameController {
                         turn_crcl.setFill(Color.YELLOW);
                     }
 
-                    for (Coin c : coins){
-
-                        if (c.type.equals(turn)){
-                            Integer position = c.position;
-                            String pos_text = "";
-                            if (position == 0) {
-                                pos_text = " ";
-                            }else {
-                                pos_text = position.toString();
-                            }
-                            if (c.id == 1) {
-                                coin_1_text.setText(pos_text);
-                            }else if (c.id == 2) {
-                                coin_2_text.setText(pos_text);
-                            }else if (c.id == 3) {
-                                coin_3_text.setText(pos_text);
-                            }else if (c.id == 4) {
-                                coin_4_text.setText(pos_text);
-                            }
-
+                    for (Coin c : this_type_coins){
+                        Integer position = c.abs_position();
+                        String pos_text = "";
+                        if (c.position == 0) {
+                            pos_text = " ";
                         }
+                        else if (position > 100) {
+                            Integer posss = position - 100;
+                            pos_text = "#" + posss.toString();
+                        }
+                        else {
+                            pos_text = position.toString();
+                        }
+
+                        if (c.id == 1) {
+                            coin_1_text.setText(pos_text);
+                        }else if (c.id == 2) {
+                            coin_2_text.setText(pos_text);
+                        }else if (c.id == 3) {
+                            coin_3_text.setText(pos_text);
+                        }else if (c.id == 4) {
+                            coin_4_text.setText(pos_text);
+                        }
+
+
                     }
                     return;
                 }
@@ -138,7 +199,6 @@ public class GameController {
                         coin.game = session.game;
                         coin.type = 1;
                         coin.position = 0;
-                        coin.abs_position = 0;
                         coins.add(coin);
                     }
                 }
@@ -150,7 +210,6 @@ public class GameController {
                         coin.game = session.game;
                         coin.type = 2;
                         coin.position = 0;
-                        coin.abs_position = 0;
                         coins.add(coin);
 
                     }
@@ -163,7 +222,6 @@ public class GameController {
                         coin.game = session.game;
                         coin.type = 3;
                         coin.position = 0;
-                        coin.abs_position = 0;
                         coins.add(coin);
 
                     }
@@ -176,7 +234,6 @@ public class GameController {
                         coin.game = session.game;
                         coin.type = 4;
                         coin.position = 0;
-                        coin.abs_position = 0;
                         coins.add(coin);
 
                     }
@@ -199,41 +256,192 @@ public class GameController {
         Button button = (Button) clickEvent.getTarget();
         String button_text = button.getText();
         if (button_text.equals("Next")){
-            dice = 0;
-            setTurn();
-            return;
+            ArrayList<Coin> this_type_coins = new ArrayList<Coin>();
+            for (Coin c : coins) {
+                if (c.type.equals(turn)) {
+                    this_type_coins.add(c);
+                }
+            }
+            Boolean win = false;
+            for (Coin c : this_type_coins) {
+                if (c.can_move_by_dice(dice, coins)) {
+                    win = true;
+                    break;
+                }else {
+                    win = false;
+                }
+            }
+            if (win) {
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Error");
+                alert.setHeaderText("You can move, MOVE!");
+                alert.showAndWait();
+                return;
+            }
+            else {
+                dice = 0;
+                setTurn();
+                return;
+            }
         }else {
-            String id = button_text.split(" ")[1] ;
+            String id = button_text.split(" ")[1];
             Integer coin_id = Integer.parseInt(id);
             Coin use_coin = new Coin();
-            for (Coin c : coins){
+            for (Coin c : coins) {
 //                System.out.println(c.id);
 ////                System.out.println(c.type);
 //                System.out.println(coin_id);
 ////                System.out.println(turn);
 
-                if (c.id.equals(coin_id) && c.type.equals(turn)){
+                if (c.id.equals(coin_id) && c.type.equals(turn)) {
                     use_coin = c;
                 }
             }
-            if (use_coin.position == 0){
-                if (dice == 6){
-                    use_coin.position = 1;
-                    use_coin.abs_position = 1;
+            if (use_coin.position == 0) {
+                if (dice == 6) {
+                    if (use_coin.can_move(1, coins)) {
+                        use_coin.position = 1;
+                    }else {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Error");
+                        alert.setHeaderText("You can't move this coin");
+                        alert.showAndWait();
+                        return;
+                    }
+                    Integer position = use_coin.abs_position();
+                    String pos_text = "";
+                    if (use_coin.position == 0) {
+                        pos_text = " ";
+                    }
+                    else if (position > 100) {
+                        Integer posss = position - 100;
+                        pos_text = "#" + posss.toString();
+                    }
+                    else {
+                        pos_text = position.toString();
+                    }
+
+
+                    if (use_coin.id == 1) {
+                        if (use_coin.type == 1) {
+                            green_coin_1.setText(pos_text);
+                        } else if (use_coin.type == 2) {
+                            red_coin_1.setText(pos_text);
+                        } else if (use_coin.type == 3) {
+                            blue_coin_1.setText(pos_text);
+                        } else if (use_coin.type == 4) {
+                            yellow_coin_1.setText(pos_text);
+                        }
+                    } else if (use_coin.id == 2) {
+                        if (use_coin.type == 1) {
+                            green_coin_2.setText(pos_text);
+                        } else if (use_coin.type == 2) {
+                            red_coin_2.setText(pos_text);
+                        } else if (use_coin.type == 3) {
+                            blue_coin_2.setText(pos_text);
+                        } else if (use_coin.type == 4) {
+                            yellow_coin_2.setText(pos_text);
+                        }
+                    } else if (use_coin.id == 3) {
+                        if (use_coin.type == 1) {
+                            green_coin_3.setText(pos_text);
+                        } else if (use_coin.type == 2) {
+                            red_coin_3.setText(pos_text);
+                        } else if (use_coin.type == 3) {
+                            blue_coin_3.setText(pos_text);
+                        } else if (use_coin.type == 4) {
+                            yellow_coin_3.setText(pos_text);
+                        }
+                    } else if (use_coin.id == 4) {
+                        if (use_coin.type == 1) {
+                            green_coin_4.setText(pos_text);
+                        } else if (use_coin.type == 2) {
+                            red_coin_4.setText(pos_text);
+                        } else if (use_coin.type == 3) {
+                            blue_coin_4.setText(pos_text);
+                        } else if (use_coin.type == 4) {
+                            yellow_coin_4.setText(pos_text);
+                        }
+                    }
                     dice = 0;
                     setTurn();
-                }else {
-                    dice = 0;
-                    setTurn();
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Error");
+                    alert.setHeaderText("You can't move this coin with out 6!Good luck");
+                    alert.showAndWait();
+                    return;
+
                 }
-            }else {
-
-                use_coin.position += dice;
-                use_coin.abs_position += dice;
-
+            } else {
+                Integer position = use_coin.position + dice;
+                if (use_coin.can_move(position, coins)) {
+                    use_coin.position = position;
+                }else {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Error");
+                    alert.setHeaderText("You can't move this coin");
+                    alert.showAndWait();
+                    return;
+                }
+                position = use_coin.abs_position();
+                String pos_text = "";
+                if (use_coin.position == 0) {
+                    pos_text = " ";
+                }
+                else if (position > 100) {
+                    Integer posss = position - 100;
+                    pos_text = "#" + posss.toString();
+                }
+                else {
+                    pos_text = position.toString();
+                }
+                if (use_coin.id == 1) {
+                    if (use_coin.type == 1) {
+                        green_coin_1.setText(pos_text);
+                    } else if (use_coin.type == 2) {
+                        red_coin_1.setText(pos_text);
+                    } else if (use_coin.type == 3) {
+                        blue_coin_1.setText(pos_text);
+                    } else if (use_coin.type == 4) {
+                        yellow_coin_1.setText(pos_text);
+                    }
+                } else if (use_coin.id == 2) {
+                    if (use_coin.type == 1) {
+                        green_coin_2.setText(pos_text);
+                    } else if (use_coin.type == 2) {
+                        red_coin_2.setText(pos_text);
+                    } else if (use_coin.type == 3) {
+                        blue_coin_2.setText(pos_text);
+                    } else if (use_coin.type == 4) {
+                        yellow_coin_2.setText(pos_text);
+                    }
+                } else if (use_coin.id == 3) {
+                    if (use_coin.type == 1) {
+                        green_coin_3.setText(pos_text);
+                    } else if (use_coin.type == 2) {
+                        red_coin_3.setText(pos_text);
+                    } else if (use_coin.type == 3) {
+                        blue_coin_3.setText(pos_text);
+                    } else if (use_coin.type == 4) {
+                        yellow_coin_3.setText(pos_text);
+                    }
+                } else if (use_coin.id == 4) {
+                    if (use_coin.type == 1) {
+                        green_coin_4.setText(pos_text);
+                    } else if (use_coin.type == 2) {
+                        red_coin_4.setText(pos_text);
+                    } else if (use_coin.type == 3) {
+                        blue_coin_4.setText(pos_text);
+                    } else if (use_coin.type == 4) {
+                        yellow_coin_4.setText(pos_text);
+                    }
+                }
                 dice = 0;
                 setTurn();
             }
         }
+
     }
 }
